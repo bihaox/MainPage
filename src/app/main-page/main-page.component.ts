6e31b662dd1eaa6bf720591af908d2c53917a9e4
Bihao Xu <bihaox@uci.edu>
2021-07-01 08:46:21 -0700

    version 3

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { config, S3 } from 'aws-sdk';
+import { Observable, Subject } from 'rxjs';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -9,14 +10,16 @@ export class MainPageComponent implements OnInit {
   public imageToShow:string;
   public tableData:Array<Array<string>>;
   public text:string;
+  public s1:string;
+  public tableChangeSubject = new Subject<string>();
   constructor() { }
 
   ngOnInit(): void {
-    this.Table1();
     this.getS3('data/2021_06_29/qualification_success_rate_by_region.csv');
+    this.Table1();
 
   }
-  public getS3(key) {
+  public async getS3(key) {
     config.update({
       accessKeyId: "AKIA4J6QPG3CTOIA3V7I",
       secretAccessKey: "GL/Y7pTF8BmqkwWZxvmukb2po6RKwp7mTmQTDi2t",
@@ -29,24 +32,37 @@ export class MainPageComponent implements OnInit {
       Bucket: 'mca-leadgen-analytical', // your bucket name,
       Key: key // path to the object you're looking for
       }
-    s3.getObject(getParams, function(err, data) {
-    // Handle any error and exit
-    console.log(data)
-    if (err)
-        return err;
-    });
+    // s3.getObject((data)=> {
+    //   var s1=data.Body.toString('utf-8');
+    //   this.tableChangeSubject.next(s1);
+    // })
+    try {
+      const data = await s3.getObject(getParams).promise();
+      return data.Body.toString('utf-8');
+    } catch (e) {
+      console.log("error loading from S3");
+      throw new Error(`Could not retrieve file from S3: ${e.message}`);
+    }
+
+    // s3.getObject(getParams, function(err, data) {
+
+    //   var s1=data.Body.toString('utf-8');
+    //   this.tableChangeSubject.next(s1);
+    //   if (err)
+    //       return err;
+    //   });
+    //   return data.Body.toString('utf-8');
+  }
+  public getTableChangedEvent(): Observable<string> {
+    return this.tableChangeSubject.asObservable();
   }
   public Table1():void {
     this.setText("Table 1");
-    this.setImage("https://mca-leadgen-analytical.s3.amazonaws.com/plots/2021_06_30/distribution_of_deals_over_crm_pipelines.png");
-    let s1=`creation_week,Las Vegas,Los Angeles,Modesto,Orange County,Palo Alto,Phoenix,San Diego,San Francisco
-    21,111,541,46,665,160,512,315,637
-    22,419,1651,163,2802,395,1951,957,2187
-    23,426,1793,179,2285,384,1857,981,2067
-    24,432,1745,150,2053,352,1728,999,2152
-    25,472,1849,161,2150,436,1989,1001,2300
-    26,104,393,32,608,121,418,213,457`;
-    this.setTable(this.decodeCSV(s1));
+    this.getS3('data/2021_06_29/qualification_success_rate_by_region.csv').then((data)=>{
+      this.setTable(this.decodeCSV(data));
+    });
+    
+
   }
   public Table2():void {
     this.setText("Table 2");
